f81cb6f4d5600cc74666e58723e69b7044355dbd
SidneyDaniel <sidneylopes17@outlook.com>
2022-09-28 18:00:30 -0300

    final version

--- forca/canvas.js
+++ forca/canvas.js
@@ -2,12 +2,10 @@ function desenharCanvas() {
     tela.lineWidth=8
     tela.lineCap="round"
     tela.lineJoin="round"
-    tela.fillStyle= "#F3F5FC"
-    tela.strokeStyle = "#0A3871"
-    tela.fillRect(0,0,1200,800)
+    tela.strokeStyle="#fff"
     tela.beginPath();
-    tela.moveTo(650,500)
-    tela.lineTo(900,500)
+    tela.moveTo(450,400)
+    tela.lineTo(700,400)
     tela.stroke()
     tela.closePath()
   }
@@ -18,12 +16,12 @@ function desenharCanvas() {
     tela.lineWidth=6
     tela.lineCap="round"
     tela.lineJoin="round"
-    tela.strokeStyle = "#0A3871"
+    tela.strokeStyle = "#fff"
     tela.beginPath()
     let largura=600/palavraSecreta.length
     for (let i=0;i<palavraSecreta.length;i++){
-      tela.moveTo(500+(largura*i),640)
-      tela.lineTo(550+(largura*i),640)
+      tela.moveTo(400+(largura*i),500)
+      tela.lineTo(450+(largura*i),500)
     }
     tela.stroke()
     tela.closePath()
@@ -32,10 +30,10 @@ function desenharCanvas() {
   function desenharForcaMastro(){
     tela.lineWidth=8
     tela.fillStyle= "#F3F5FC"
-    tela.strokeStyle = "#000"
+    tela.strokeStyle = "#fff"
     tela.beginPath();
-    tela.moveTo(700,190)
-    tela.lineTo(700,500)
+    tela.moveTo(500,90)
+    tela.lineTo(500,400)
     tela.stroke()
     tela.closePath()
     
@@ -44,10 +42,10 @@ function desenharCanvas() {
   function desenharForcaMastro_2(){
     tela.lineWidth=8
     tela.fillStyle= "#F3F5FC"
-    tela.strokeStyle = "#000"
+    tela.strokeStyle = "#fff"
     tela.beginPath();
-    tela.moveTo(850,190)
-    tela.lineTo(700,190)
+    tela.moveTo(650,90)
+    tela.lineTo(500,90)
     tela.stroke()
     tela.closePath()
     
@@ -56,13 +54,12 @@ function desenharCanvas() {
   function desenhaCirculo(){
     tela.lineWidth=8
     tela.fillStyle= "#F3F5FC"
-    tela.strokeStyle = "#000"
+    tela.strokeStyle = "#fff"
     tela.beginPath();
-    tela.arc(810, 260, 40, 0, Math.PI * 2, true); // Círculo 
+    tela.arc(610, 160, 40, 0, Math.PI * 2, true); // Círculo 
     tela.moveTo(110, 75);
-
-    tela.moveTo(810, 190)
-    tela.lineTo(810, 215)
+    tela.moveTo(610, 90)
+    tela.lineTo(610, 115)
     tela.stroke();
     tela.closePath()
   }
@@ -70,10 +67,10 @@ function desenharCanvas() {
   function desenharApoio(){
     tela.lineWidth=8
     tela.fillStyle= "#F3F5FC"
-    tela.strokeStyle = "#000"
+    tela.strokeStyle = "#fff"
     tela.beginPath();
-    tela.moveTo(750,190)
-    tela.lineTo(700,300)
+    tela.moveTo(550,90)
+    tela.lineTo(500,200)
     tela.stroke()
     tela.closePath()
   }
@@ -81,10 +78,10 @@ function desenharCanvas() {
   function desenhaTorso(){
     tela.lineWidth=8
     tela.fillStyle= "#F3F5FC"
-    tela.strokeStyle = "#000"
+    tela.strokeStyle = "#fff"
     tela.beginPath();
-    tela.moveTo(810,300)
-    tela.lineTo(810,400)
+    tela.moveTo(610,200)
+    tela.lineTo(610,300)
     tela.stroke()
     tela.closePath()
   }
@@ -92,10 +89,10 @@ function desenharCanvas() {
   function desenharBraco(){
     tela.lineWidth=8
     tela.fillStyle= "#F3F5FC"
-    tela.strokeStyle = "#000"
+    tela.strokeStyle = "#fff"
     tela.beginPath();
-    tela.moveTo(810,300)
-    tela.lineTo(860,350)
+    tela.moveTo(610,200)
+    tela.lineTo(660,250)
     tela.stroke()
     tela.closePath()
   }
@@ -103,10 +100,10 @@ function desenharCanvas() {
   function desenharBraco_2(){
     tela.lineWidth=8
     tela.fillStyle= "#F3F5FC"
-    tela.strokeStyle = "#000"
+    tela.strokeStyle = "#fff"
     tela.beginPath();
-    tela.moveTo(810, 300)
-    tela.lineTo(760, 350)
+    tela.moveTo(610, 200)
+    tela.lineTo(560, 250)
     tela.stroke()
     tela.closePath()
   }
@@ -114,10 +111,10 @@ function desenharCanvas() {
   function desenharPerna(){
     tela.lineWidth=8
     tela.fillStyle= "#F3F5FC"
-    tela.strokeStyle = "#000"
+    tela.strokeStyle = "#fff"
     tela.beginPath();
-    tela.moveTo(810, 400)
-    tela.lineTo(770, 450)
+    tela.moveTo(610, 300)
+    tela.lineTo(570, 350)
     tela.stroke()
     tela.closePath()
   }
@@ -125,33 +122,33 @@ function desenharCanvas() {
   function desenharPerna_2(){
     tela.lineWidth=8
     tela.fillStyle= "#F3F5FC"
-    tela.strokeStyle = "#000"
+    tela.strokeStyle = "#fff"
     tela.beginPath();
-    tela.moveTo(810, 400)
-    tela.lineTo(850, 450)
+    tela.moveTo(610, 300)
+    tela.lineTo(650, 350)
     tela.stroke()
     tela.closePath()
   }
 
 // ===================================================================== //
 
-
+//===========================Letras Corretas============================//
   function escreverLetraCorreta(index){
       tela.font = "bold 52px Serif"
       tela.lineCap = "round"
-      tela.fillStyle = "#0A3871"
+      tela.fillStyle = "#fff"
       tela.lineWidth = 6
       let largura= 600/palavraSecreta.length
-      tela.fillText(palavraSecreta[index],505 + (largura * index),620)
+      tela.fillText(palavraSecreta[index],410 + (largura * index),490)
       
   }
-  
+//=========================Letras Incorretas============================//
   function escreverLetraIncorreta(letra, erros){
-    tela.font = "bold 40px Inter"
+    tela.font = "bold 40px Serif"
     tela.lineCap = "round"
-    tela.fillStyle = "#0A3871"
+    tela.fillStyle = "#fff"
     tela.lineWidth = 6
-    tela.fillText(letra,420 + (40 *(10 - erros)),720,40 )
+    tela.fillText(letra,320 + (40 *(10 - erros)),610,40 )
   }
 
   
